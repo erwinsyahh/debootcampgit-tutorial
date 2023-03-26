6991cb85859a9b62004a51432c62736fd994703e
“Erwinsyah <erwinsyah.hartono@gmail.com>
2023-03-26 16:04:21 +0700

    add more

--- testpython.py
+++ testpython.py
@@ -13,8 +13,29 @@ class Handphone:
     def storageram(self, x, y):
         self.storage = x
         self.RAM = y
-
-
 hp1 = Handphone("Erwin","Samsung","Merah")
 hp1.storageram(8,128)
 hp1.greet()
+
+#Part2
+class Handphone:
+    def __init__(self, name, merk, warna, storage, RAM, toko="Toped"):
+        self.name = name
+        self.merk = merk
+        self.warna = warna
+        self.storage = f"{storage} GB"
+        self.RAM = f"{RAM} GB"
+        self.toko = toko
+    
+    def greet(self):
+        print(f"Hello, my name is {self.name}")
+        #print("This phone's specs are:", self.name, self.merk, self.warna, self.storage, self.RAM, "from", self.toko)
+        print(f"This phone's specs are: {self.name} {self.merk} {self.warna} {self.storage} {self.RAM} from {self.toko}")
+    
+    def merk_length(self):
+        return len(self.merk)
+    
+hp1 = Handphone("Erwin", "Xiaomi", "Black", 256, 16)
+hp2 = Handphone("Desy", "Samsung", "White", 128, 8, "Shopee")
+hp1.greet()
+hp2.greet()
